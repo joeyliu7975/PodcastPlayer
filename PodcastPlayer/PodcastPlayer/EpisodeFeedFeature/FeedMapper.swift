fee41ff1fd794d48c60f640fa1b225e1b1c02779
JiaXin Liu <joeyliu7975@gmail.com>
2021-03-03 16:20:02 +0800

    Seperate protocol FeedMapper to FeedMapper.swift file

---
+++ PodcastPlayer/PodcastPlayer/EpisodeFeedFeature/FeedMapper.swift
@@ -0,0 +1,13 @@
+//
+//  FeedMapper.swift
+//  PodcastPlayer
+//
+//  Created by Joey Liu on 3/3/21.
+//
+
+import Foundation
+
+// 我在這邊只會拿到 Data 和 response ,其他部分將會有 conform FeedMapper 的 class 去實作
+public protocol FeedMapper {
+    func map(data:Data, response: HTTPURLResponse) throws -> ChannelFeed
+}
