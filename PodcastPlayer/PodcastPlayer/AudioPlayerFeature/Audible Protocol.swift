28e2096dd419593f4857cc473c2fb2f09bdbbb7f
JiaXin Liu <joeyliu7975@gmail.com>
2021-03-03 10:54:34 +0800

    Declare protocol Playalbe, Controller and a protocol AudibleProtocol that conform Playable and Controllable

--- PodcastPlayer/PodcastPlayer/AudioPlayerFeature/Audible Protocol.swift
+++ PodcastPlayer/PodcastPlayer/AudioPlayerFeature/Audible Protocol.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+public protocol Playable {
+    func play()
+    func pause()
+}
+// Audio 進度調整和前一集、下一集
+public protocol Controllable {
+    associatedtype Number
+    typealias Completion = (Episode, Int) -> Void
+    
+    func progressControl(value: Number)
+    func nextEp(currentEpisode: Int, completion: Completion)
+    func previousEp(currentEpisode: Int, completion: Completion)
+}
+
+public protocol AudioProtocol: Playable, Controllable {
+    var currentEpisodeIndex: Int? { get set }
+    var currentEpisode: Episode? { get set }
+}
