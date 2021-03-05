4b11055703137e488b1298e29ecc20c06b06cdd6
JiaXin Liu <joeyliu7975@gmail.com>
2021-03-05 10:04:43 +0800

    Convert Audible Protocol swift code to objective-C

---
+++ PodcastPlayer/PodcastPlayer/AudioPlayerFeature/Objective/AudibleProtocol.h
@@ -0,0 +1,23 @@
+//
+//  AudibleProtocol.h
+//  PodcastPlayer
+//
+//  Created by Joey Liu on 3/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol PlayPauseProtocol <NSObject>
+    -(void)play;
+    -(void)pause;
+@end
+
+@protocol EpisodeProgressTracking <NSObject>
+    -(void)updatEpisodeCurrentDurationWith:(float)value;
+@end
+
+@protocol EpisodeSoundLoader <NSObject>
+    -(void)loadWith:(NSString)urlName;
+@end
