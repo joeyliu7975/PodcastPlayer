40fb0e8c396eed7215e5eee02304e72406a421dc
JiaXin Liu <joeyliu7975@gmail.com>
2021-03-06 22:32:55 +0800

    Test function 'getSoundURl': test case return soundURL

--- PodcastPlayer/PlayerModelControllerTests/PlayerModelControllerTests.swift
+++ PodcastPlayer/PlayerModelControllerTests/PlayerModelControllerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import PodcastPlayer
 
 class PlayerModelControllerTests: XCTestCase {
-
+    //MARK: Test function getSoundURL
     func test_getSoundURL_NoSoundURLError() {
         let episode = anyEpisode(soundURL: nil)
         let sut = PlayerModelController(episodes: [episode], currentIndex: 0)
@@ -18,6 +18,25 @@ class PlayerModelControllerTests: XCTestCase {
         
         XCTAssertThrowsError(try sut.getSoundURL(with: episode), "expect \(expectedError), but get return value instead.") { (error) in
             XCTAssertEqual((error as? PlayerModelController.Error), expectedError)
+        }
+    }
+    
+    func test_getSoundURL_hasSoundURL() {
+        let expectedURL = anySoundURL()
+        let episode = anyEpisode(soundURL: expectedURL)
+        let sut = PlayerModelController(episodes: [episode], currentIndex: 0)
+        
+        guard let result = try? sut.getSoundURL(with: episode) else {
+            XCTFail("Error")
+            return
+        }
+        
+        switch result {
+        case let .success((_, receivedURL)):
+            XCTAssertNotNil(receivedURL)
+            XCTAssertEqual(expectedURL, receivedURL)
+        default:
+            XCTFail("Expect\(expectedURL), but it throw error instead")
         }
     }
    
