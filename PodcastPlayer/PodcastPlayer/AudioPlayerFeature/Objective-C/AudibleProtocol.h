8df1b725e5db34ae8bfc7e1c4e3c59eb2d3dfd29
JiaXin Liu <joeyliu7975@gmail.com>
2021-03-07 10:29:29 +0800

    Trying to convert AudioPlayerController to objective-c .h file

--- PodcastPlayer/PodcastPlayer/AudioPlayerFeature/Objective-C/AudibleProtocol.h
+++ PodcastPlayer/PodcastPlayer/AudioPlayerFeature/Objective-C/AudibleProtocol.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -22,4 +23,22 @@ NS_ASSUME_NONNULL_BEGIN
     -(void)loadWith:(NSURL *)url;
 @end
 
+@interface AudioPlayer: NSObject
+
+@property (strong, nonatomic) id *timeObserver;
+@property (strong, nonatomic) BOOL *isSeekInProgress;
+
+@property (strong, nonatomic) AVPlayerItem.Status *playerCurrentItemStatus;
+
+@property (strong, nonatomic) CMTime *chaseTime;
+
+@property (nonatomic, copy) void (^trackDuration)((float,float));
+
+@property (nonatomic, copy) void (^playNextEP)(void);
+
+@property (nonatomic, copy) void(^updateProgress)(BOOL);
+
+- (void)resetPlayer:void;
+
+@end
 NS_ASSUME_NONNULL_END
