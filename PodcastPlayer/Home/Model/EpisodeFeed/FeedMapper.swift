4d87b7d1498d8b4ef682c5384864b33153d49abf
Joey <joeyliu@kkbox.com>
2021-05-01 17:05:41 +0800

    Uncomment protocol FeedMapper

--- PodcastPlayer/Home/Model/EpisodeFeed/FeedMapper.swift
+++ PodcastPlayer/Home/Model/EpisodeFeed/FeedMapper.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 // 我在這邊只會拿到 Data 和 response ,其他部分將會有 conform FeedMapper 的 class 去實作
-//public protocol FeedMapper {
-//    func map(data:Data, response: HTTPURLResponse) throws -> ChannelFeed
-//}
+public protocol FeedMapper {
+    func map(data:Data, response: HTTPURLResponse) throws -> ChannelFeed
+}
